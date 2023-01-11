6adbcfff1f659158debcb39becf0cbf244e2d42f
Kinda-Nefarious <rufaronyakudya@gmail.com>
2023-01-11 02:48:23 -0800

    One more try

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+'''Module for saving argv info via json to file.'''
 
-"""Add all arguments to a Python list and save them to a file."""
-import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+from sys import argv
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+MyList = argv[1:]
+try:
+    my_obj = load_from_json_file(filename)
+    my_obj += MyList
+    save_to_json_file(my_obj, filename)
+except Exception as e:
+    my_obj = MyList
+    save_to_json_file(my_obj, filename)
